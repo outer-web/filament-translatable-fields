5a3647dfe80222e19488405637f96079153006f9
GoodM4ven <goodm4ven@proton.me>
2025-04-01 14:48:33 +0300

    Fix IDE intellisense

--- .ide-helper/_field_macros.php
+++ .ide-helper/_field_macros.php
@@ -3,7 +3,7 @@
 namespace Filament\Forms\Components;
 
 /**
- * @method static static translatable(bool $translatable = true, ?array $customLocales = null, ?array $localeSpecificRules = null)
+ * @method Field translatable(bool $translatable = true, ?array $customLocales = null, ?array $localeSpecificRules = null)
  * 
  * @see \Outerweb\FilamentTranslatableFields\Filament\Plugins\FilamentTranslatableFieldsPlugin::boot()
  */
